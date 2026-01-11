5ded525e4e38ae5202f9dfeda15e7ac947837af8
abdujabbarovsamandar0100@gmail.com.noreply.github.com <abdujabbarovsamandar0100@gmail.com>
2026-01-11 12:53:59 +0900

    keyranes added for dropdown

--- dropdown-menu/script.js
+++ dropdown-menu/script.js
@@ -3,12 +3,13 @@ const menuTitle = document.querySelector(".menu-title");
 const dropdownMenu = document.querySelector(".dropdown-menu");
 
 menuTitle.addEventListener("click", (e) => {
-  e.stopPropagation(); // prevents window click from firing
-  dropdownMenu.classList.toggle("show");
-});
+  if (e.target === e.currentTarget) {
+    dropdownMenu.classList.toggle("visible");
+  }  
+})
 
 window.addEventListener("click", (e) => {
   if (!dropdownContainer.contains(e.target)) {
-    dropdownMenu.classList.remove("show");
+    dropdownMenu.classList.remove("visible")
   }
-});
+})
